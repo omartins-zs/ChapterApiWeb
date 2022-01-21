16b933b2431f521ee15de6a4ca8ecfccf0d42a4a
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-21 20:21:38 -0300

    Iniciando Criação Model Usuarios

---
+++ ChapterApiWeb/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChapterApiWeb.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public int Tipo { get; set; }
+
+    }
+}
