60aae58e3dd0c547e32cadd8a6e78ab37bcf7c90
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-27 19:57:13 -0300

    Criando Controllador UsuariosController

---
+++ ChapterApiWeb/Controllers/UsuariosController.cs
@@ -0,0 +1,27 @@
+﻿using ChapterApiWeb.Models;
+using ChapterApiWeb.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChapterApiWeb.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuariosController(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        
+}
