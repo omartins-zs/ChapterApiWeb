eff58c656c56689ec1bc35c63f68ea4b21c1d552
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-27 17:23:11 -0300

    Criando Metodos HTTP Na Classe UsuarioRepository

---
+++ ChapterApiWeb/Repositories/UsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using ChapterApiWeb.Contexts;
+using ChapterApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChapterApiWeb.Repositories
+{
+    public class UsuarioRepository
+    {
+        // possui acesso aos dados -> Somente leitura
+        private readonly ChapterContext _context;
+
+        public UsuarioRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+       
+    }
+}
