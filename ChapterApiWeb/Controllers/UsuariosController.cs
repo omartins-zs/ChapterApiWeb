a777772aeb1c121db7ad0bbc7633e8d5fe78380a
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-27 19:58:05 -0300

    Criando Routes HTTP no Controllador Usuarios

--- ChapterApiWeb/Controllers/UsuariosController.cs
+++ ChapterApiWeb/Controllers/UsuariosController.cs
@@ -23,5 +23,91 @@ namespace ChapterApiWeb.Controllers
             _usuarioRepository = usuarioRepository;
         }
 
-        
+        // Get
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_usuarioRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        //  Get By Id
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Usuario usuarioProcurado = _usuarioRepository.BuscarPorId(id);
+
+                if (usuarioProcurado == null)
+                {
+                    //Mensagem dentro do retturn colocar no final
+                    return NotFound("Usuario não encontrado!");
+                }
+
+                return Ok(usuarioProcurado);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        // Post
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario u)
+        {
+            try
+            {
+                _usuarioRepository.Cadastrar(u);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        // Put
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Usuario u)
+        {
+            try
+            {
+                _usuarioRepository.Atualizar(id, u);
+
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        // Delete
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _usuarioRepository.Deletar(id);
+
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
 }
