f6eabee7b8a47ad1d5e81a78ebfa4503c2cbd4a5
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-04 15:03:52 -0300

    Criando Verificação Pre Login

--- ChapterApiWeb/Controllers/LoginController.cs
+++ ChapterApiWeb/Controllers/LoginController.cs
@@ -1,11 +1,44 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ChapterApiWeb.Models;
+using ChapterApiWeb.Repositories;
+using ChapterApiWeb.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace ChapterApiWeb.Controllers
 {
+    // Para informar que estamos trabalhando com Json
+    [Produces("application/json")]
+
     [Route("api/[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly UsuarioRepository _usuarioRepository;
+        public LoginController(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        // Mesmo sendo Login uma Consulta normante usamos get dessa vez usar o post pela seguranca de os dados nao irem pela Url.
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                // Verificaçao pré Login
+                Usuario usuarioBuscado = _usuarioRepository.Login(login.email, login.senha);
+
+                if (usuarioBuscado == null)
+                {
+                    return NotFound("E-mail e/ou senha invalidos");
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
