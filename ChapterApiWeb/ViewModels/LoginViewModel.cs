4de694c481c84e113f5128d918f2d2cf0c053fde
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-04 14:09:35 -0300

    Adicionando Mensagem No Required Anottations

--- ChapterApiWeb/ViewModels/LoginViewModel.cs
+++ ChapterApiWeb/ViewModels/LoginViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace ChapterApiWeb.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChapterApiWeb.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Informe o e-mail do usuário")]
+        public string email { get; set; }
+
+        public string senha { get; set; }
+
     }
 }
