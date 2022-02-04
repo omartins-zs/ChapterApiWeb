7284a043d95b751d4dc94e24a913b1f886cae1ee
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-04 14:10:38 -0300

    Adicionando Mensagem de Anottations na senha

--- ChapterApiWeb/ViewModels/LoginViewModel.cs
+++ ChapterApiWeb/ViewModels/LoginViewModel.cs
@@ -7,6 +7,7 @@ namespace ChapterApiWeb.ViewModels
         [Required(ErrorMessage = "Informe o e-mail do usuário")]
         public string email { get; set; }
 
+        [Required(ErrorMessage = "Informe a senha do usuário")]
         public string senha { get; set; }
 
     }
