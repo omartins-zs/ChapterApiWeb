15197e288f70bd45318b931ee4ea4dfef5b061fd
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-04 17:00:50 -0300

    Ajuste para Realizar Test no LoginControllerTeste

--- ChapterApiWeb/ViewModels/LoginViewModel.cs
+++ ChapterApiWeb/ViewModels/LoginViewModel.cs
@@ -10,5 +10,14 @@ namespace ChapterApiWeb.ViewModels
         [Required(ErrorMessage = "Informe a senha do usuário")]
         public string senha { get; set; }
 
+
+        // Criado para Realizar Teste e usar no LoginControllerTeste e IUsuarioRepository
+
+
+        [Required(ErrorMessage = "Informe o e-mail do usuário")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha do usuário")]
+        public string Senha { get; set; }
     }
 }
